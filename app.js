05c7e890def31fd226663398f518f53f636db550
harsh-haria <harshharia1234@gmail.com>
2022-07-30 13:47:45 +0530

    sending email complete

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const MongoDBStore = require('connect-mongodb-session')(session);
 const express = require("express");
 const bodyParser = require('body-parser');
 const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const MONGODB_URI = require('./util/database');
 
@@ -41,6 +42,7 @@ app.use(
   })
 );
 app.use(csrfProtection);
+app.use(flash());
 
 app.use( (req,res,next) => {
   if(!req.session.user){
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,21 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const nodeMailer = require("nodemailer");
+
+const emailer = require("../util/email");
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     pageTitle: "Login",
     path: "/login",
     isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -13,10 +23,11 @@ exports.postLogin = (req, res, next) => {
   // res.setHeader('Set-Cookie','loggedIn=true');
   const email = req.body.email;
   const password = req.body.password;
-  
+
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "Invalid Email or Password!");
         return res.redirect("/login");
       }
       bcrypt
@@ -30,7 +41,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-
+          req.flash("error", "Invalid Email or Password!");
           return res.redirect("/login");
         })
         .catch((err) => console.log(err));
@@ -46,10 +57,17 @@ exports.postLogout = (req, res, next) => {
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "Signup",
     isAuthenticated: false,
+    errorMessage: message,
   });
 };
 
@@ -61,6 +79,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash("error", "User already Exists!");
         return res.redirect("/signup");
       }
       return bcrypt
@@ -76,8 +95,24 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
-        });
+          message={
+            to: email,
+            from: {
+              name:'SmartShoper',
+              email: emailer.emailHost
+            },
+            subject: "Signup Completed",
+            text: 'Congratulatoions. You are now successfully registered on SmartShoper',
+            html: "<h1>Congratulatoions</h1><br><p>You are now successfully registered on SmartShoper. </p>",
+          }
+          return emailer.setup
+            .send(message)
+            .then((response) => {
+              console.log("Email sent");
+            })
+            .catch((err) => console.log(err));
+        })
+        .catch((err) => console.log(err));
     })
-
     .catch((err) => console.log(err));
 };
