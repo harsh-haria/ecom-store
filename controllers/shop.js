c8bef88987e873476a08dfaddf81b0fc263e252c
harsh-haria <harshharia1234@gmail.com>
2022-08-10 22:27:51 +0530

    Pagination added

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,18 +8,36 @@ const Order = require("../models/order");
 
 const rootdir = require("../util/path");
 
+const ITEMS_PER_PAGE = 2;
+
 exports.getIndexPage = (req, res, next) => {
   // res.setHeader('Set-Cookie','loggedIn=false');
+  const page = +req.query.page || 1;
+  let totalItems;
   Product.find()
-    .then((data) => {
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
+    .then((products) => {
       res.render("shop/index", {
-        product: data,
-        pageTitle: "Home",
+        prods: products,
+        pageTitle: "Shop",
         path: "/",
+        totalProducts: totalItems,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page>1,
+        nextPage: page+1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems/ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
-      // console.log(err);
+      console.log(err);
       const error = new Error(err);
       error.httpStatusCode = 500;
       return next(error);
